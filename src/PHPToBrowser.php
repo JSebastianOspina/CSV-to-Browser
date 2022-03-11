ad16484a6f74fe515a98282b25a2d65879c3a402
Sebastian Ospina <sebastianospinacontacto@gmail.com>
2022-03-11 15:54:29 -0500

    Version 1.0

---
+++ src/PHPToBrowser.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ *
+ */
+class PHPToBrowser
+{
+    /**
+     * @var
+     */
+    private $headerColumns;
+    /**
+     * @var
+     */
+    private $bodyColumns;
+
+    /**
+     * @var string
+     */
+    private $separator;
+
+    /**
+     * @param $headerColumns
+     * @param String|null $separator
+     */
+    public function __construct($headerColumns, string $separator = null)
+    {
+        $this->headerColumns = $headerColumns;
+        $this->separator = $separator ?? ';';
+    }
+
+    /**
+     * @param $data
+     * @return void
+     */
+    public function addRow($data): void
+    {
+        if (count($data) < count($this->headerColumns)) {
+            $difference = count($this->headerColumns) - count($data);
+            for ($i = 0; $i <= $difference; $i++) {
+                $data[] = 'No data provided for this column';
+            }
+        }
+        $this->bodyColumns[] = $data;
+    }
+
+    /**
+     * @param string $outputName The filename output
+     * @return void
+     */
+    public function generateCSV(string $outputName)
+    {
+        header('Content-Type: text/csv');
+        header('Content-Disposition: attachment;filename=' . $outputName . 'csv');
+        $fp = fopen('php://output', 'wb');
+        //Write header
+        fputcsv($fp, $this->headerColumns, ';');
+        foreach ($this->bodyColumns as $row) {
+            fputcsv($fp, $row, $this->separator);
+        }
+        fclose($fp);
+    }
+
+
+}
